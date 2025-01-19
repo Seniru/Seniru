def3146461ffbba34b8b669a8139732b484a8dc9
Seniru <senirupasan@gmail.com>
2025-01-19 18:54:25 +0530

    chore: Implement blog editting in frontend

--- src/routes/blogs/[blog]/+page.svelte
+++ src/routes/blogs/[blog]/+page.svelte
@@ -6,12 +6,19 @@
     import BlogPost from "../../../components/BlogPost.svelte"
 
     export let data: { blog: BlogPost }
+
     let blog = null
+    let pageViews = 0
+    let editting = false
+    let title = ""
+    let content = ""
+
     if (data) {
         blog = data.blog.result
+        title = blog.title
+        content = blog.content
     }
 
-    let pageViews = 0
 
     onMount(async () => {
         let response = await fetch(`https://api.counterapi.dev/v1/seniru/blog-${blog.id}/up`)
@@ -26,6 +33,20 @@
             method: "DELETE"
         })
         if (response.ok) window.location.href = "/blogs"
+    }
+
+    const editBlog = async (event: SubmitEvent) => {
+        event.preventDefault()
+        let response = await fetch(`/api/blogs/${blog.id}`, {
+            method: "PATCH",
+            body: JSON.stringify({ title, content })
+        })
+        let result = await response.json()
+        if (response.ok) {
+            window.location.reload()
+        } else {
+            alert(result.message || response.statusText)
+        }
     }
 
 </script>
@@ -69,9 +90,46 @@
 <br />
 <br />
 
-{#if $page.data.priviledged}
-    <button on:click={deleteBlog}>Delete</button>
+{#if editting}
+    <h1>Edit blog post</h1>
+
+    <form on:submit={editBlog}>
+        <label for="title">Title</label>
+        <br />
+        <input type="text" name="title" bind:value={title} />
+        <br />
+        <br />
+        <label for="body">Body</label>
+        <br />
+        <textarea name="body" cols={100} rows={10} bind:value={content} contenteditable />
+        <br />
+        <input type="submit" value="Submit" />
+        <button type="button" on:click={() => editting = false}>
+            Cancel
+        </button>
+    </form>
+
+    <br />
+    <h3>Preview</h3>
+    <hr />
+    <br />
+
+    {#if content.length > 0}
+        <div class="container">
+            {@html marked.parse(content)}
+        </div>
+    {:else}
+        <p class="important">Nothing to display yet...</p>
+    {/if}
 {/if}
+
+{#if $page.data.priviledged && !editting}
+    <div class="priviledged-actions">
+        <button on:click={() => editting = true}>Edit</button>
+        <button style="background-color: red;" on:click={deleteBlog}>Delete</button>
+    </div>
+{/if}
+
 
 <style lang="scss">
     @use "../../../utils/variables.scss" as v;
@@ -84,4 +142,10 @@
         justify-content: space-between;
         flex-wrap: wrap;
     }
+
+    .priviledged-actions {
+        display: flex;
+        justify-content: space-between;
+    }
+
 </style>
