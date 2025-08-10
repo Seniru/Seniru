560fb693fae06ebed3b0a9afa6b91722340bfe1f
Seniru <senirupasan@gmail.com>
2025-08-10 17:41:39 +0530

    feat: implement reply submit form

---
+++ src/components/BlogPostReply.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+    export let nickname: string
+    export let content: string
+</script>
+
+<div class="container reply-container">
+    <div class="nick">
+        {#if nickname === "senirupasan"}
+            <b>senirupasan</b>
+            <abbr title="This is a verified reply made by the author">(author)</abbr>
+            replied,
+        {:else}
+            <b>{nickname}</b> replied,
+        {/if}
+    </div>
+    <p>{content}</p>
+</div>
+
+<style lang="scss">
+    @use "../utils/variables.scss" as v;
+
+    .reply-container {
+        margin-bottom: 10px;
+        margin-top: 10px;
+
+        .nick {
+            font-style: italic;
+            color: v.$text-color-secondary;
+        }
+    }
+</style>
