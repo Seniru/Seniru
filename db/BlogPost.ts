f7b8572326b1369a52318d182ac98f4dcc43bac7
Seniru <senirupasan@gmail.com>
2025-08-10 17:22:48 +0530

    feat: create POST /blogs/{:id}/reply

--- db/BlogPost.ts
+++ db/BlogPost.ts
@@ -1,7 +1,13 @@
+export interface BlogPostReply {
+    readonly nickname: string
+    readonly replyContent: string
+}
+
 export interface BlogPost {
     readonly id: number
     readonly title: string
     readonly content?: string
     readonly publishedDate: Date
     readonly lastEdittedDate?: Date
+    readonly replies?: BlogPostReply[]
 }
